9415cc17966d0c4151a75bedf96cd89544a53dfb
A.Aydogan Asus Desktop <aaad@itu.edu.tr>
2021-02-25 14:48:33 +0300

    development continued

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('js', function () {
 //Font Awesome
 gulp.task('fa', function () {
     return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
-        .pipe(gulp.dest("app/css/fonts"))
+        .pipe(gulp.dest("app/fonts"))
         .pipe(browserSync.reload({
             stream: true
         }))
